2c5f51ac29f5551aa960e61560d6ddd999615105
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2023-08-15 18:28:04 -0300

    add safecall, try to fix https://github.com/game-ci/unity-builder/issues/439

--- dist/platforms/windows/entrypoint.ps1
+++ dist/platforms/windows/entrypoint.ps1
@@ -1,3 +1,40 @@
+# Call a function, and exit on total timeout, or no output in a period of time
+function SafeCall($Code, $NoOutputTimeout, $Timeout) {
+    $NoOutputChecks = $Timeout / $NoOutputTimeout
+    $Process = Start-Job -ScriptBlock $code
+
+    $NoOutputChecks
+
+    $i = 0
+    While ($true) {
+        if ($i -eq $NoOutputChecks) {
+            Write-Output "$('SafeCall> Process cancelled by total timeout.')"
+            Break    
+        }
+        $i += 1
+
+        if (Wait-Job $Process -Timeout $NoOutputTimeout) {
+            Write-Output "$('SafeCall> Process Finished')"
+            Receive-Job $Process
+            Break
+        } else {
+            $Output = Receive-Job $Process
+            $Output
+            if ($Output.Length -gt 0) {
+                Write-Output "$('SafeCall> There is output. Keep Going.')"
+            } else {
+                Write-Output "$('SafeCall> No output. Cancelling process.')"
+                Break
+            }
+        }
+    }
+
+    Write-Output "$('SafeCall> Process finish state: ')$($Process.State)"
+    $Process.State
+
+    Remove-Job -force $Process
+}
+
 # Import any necessary registry keys, ie: location of windows 10 sdk
 # No guarantee that there will be any necessary registry keys, ie: tvOS
 Get-ChildItem -Path c:\regkeys -File | Foreach {reg import $_.fullname}
@@ -5,28 +42,27 @@ Get-ChildItem -Path c:\regkeys -File | Foreach {reg import $_.fullname}
 # Register the Visual Studio installation so Unity can find it
 regsvr32 C:\ProgramData\Microsoft\VisualStudio\Setup\x64\Microsoft.VisualStudio.Setup.Configuration.Native.dll
 
+$timeoutSeconds = $Env:BUILD_TIMEOUT * 60
+
 # Setup Git Credentials
 & "c:\steps\set_gitcredential.ps1"
 
 # Activate Unity
-& "c:\steps\activate.ps1"
+$ActivateCode = {
+    & "c:\steps\activate.ps1"
+}
 
 # Build with timeout
-$timeoutSeconds = $Env:BUILD_TIMEOUT * 60
-$code = {
+$BuildCode = {
     # Build the project
     & "c:\steps\build.ps1"
 }
-Write-Output "$('> Start Job')"
-$j = Start-Job -ScriptBlock $code
-if (Wait-Job $j -Timeout $timeoutSeconds) {
-    Receive-Job $j
-    Write-Output "$('> Job Completed')"
-} else {
-    Receive-Job $j
-    Write-Output "$('> Job Interrupted')"
-}
-Remove-Job -force $j
 
 # Free the seat for the activated license
-& "c:\steps\return_license.ps1"
+$ReturnLicenseCode = {
+    & "c:\steps\return_license.ps1"
+}
+
+SafeCall $ActivateCode 120 $timeoutSeconds
+SafeCall $BuildCode 120 $timeoutSeconds
+SafeCall $ReturnLicenseCode 120 $timeoutSeconds
