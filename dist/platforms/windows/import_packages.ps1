7e9120bbb5b8a925e1ca91b1b944d291b512aafd
Vitaly Popuzin <popuzin@gmail.com>
2024-01-17 10:38:17 +0100

    add skip step

--- dist/platforms/windows/import_packages.ps1
+++ dist/platforms/windows/import_packages.ps1
@@ -1,20 +1,24 @@
-# import_package.ps1
 # Imports Unity packages from the specified directory
 
 $UnityExecutable = "C:\Program Files\Unity\Hub\Editor\$Env:UNITY_VERSION\Editor\Unity.exe"
 $UnityProjectPath = "$Env:GITHUB_WORKSPACE\$Env:PROJECT_PATH"
 $RequiredPackagesPath = "$UnityProjectPath\Assets\RequiredPackages\requiredPackages.unitypackage"
 
-# Import the package using Unity command line
-& $UnityExecutable -batchmode -quit -nographics `
-                    -projectPath $UnityProjectPath `
-                    -importPackage $RequiredPackagesPath `
-                    -logfile | Out-Host
+# Check if the required packages file exists
+if (Test-Path -Path $RequiredPackagesPath) {
+  # Import the package using Unity command line
+  & $UnityExecutable -batchmode -quit -nographics `
+                        -projectPath $UnityProjectPath `
+                        -importPackage $RequiredPackagesPath `
+                        -logfile | Out-Host
 
-# Check for any errors
-if ($LastExitCode -ne 0) {
+  # Check for any errors
+  if ($LastExitCode -ne 0) {
     Write-Output "Package import failed with exit code $LastExitCode"
     exit $LastExitCode
-} else {
+  } else {
     Write-Output "Package import succeeded"
+  }
+} else {
+  Write-Output "Package file not found at $RequiredPackagesPath, skipping import step."
 }
